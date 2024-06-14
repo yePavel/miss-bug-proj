ca6b4c63fa3c4f578af9b3bd9953cab1836b4f32
pavel <pavel.yv@gmail.com>
2024-06-14 17:03:36 +0300

    Init repo

---
+++ cmps/BugList.jsx
@@ -0,0 +1,32 @@
+const {Link} = ReactRouterDOM
+
+import {BugPreview} from './BugPreview.jsx'
+
+export function BugList({bugs, onRemoveBug, onEditBug}) {
+  return (
+    <ul className="bug-list">
+      {bugs.map((bug) => (
+        <li className="bug-preview" key={bug._id}>
+          <BugPreview bug={bug} />
+          <div>
+            <button
+              onClick={() => {
+                onRemoveBug(bug._id)
+              }}
+            >
+              x
+            </button>
+            <button
+              onClick={() => {
+                onEditBug(bug)
+              }}
+            >
+              Edit
+            </button>
+          </div>
+          <Link to={`/bug/${bug._id}`}>Details</Link>
+        </li>
+      ))}
+    </ul>
+  )
+}
---
+++ pages/BugIndex.jsx
@@ -0,0 +1,77 @@
+import { bugService } from '../services/bug.service.js'
+import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service.js'
+import { BugList } from '../cmps/BugList.jsx'
+
+const { useState, useEffect } = React
+
+export function BugIndex() {
+  const [bugs, setBugs] = useState([])
+
+  useEffect(() => {
+    loadBugs()
+  }, [])
+
+  function loadBugs() {
+    bugService.query().then(setBugs)
+  }
+
+  function onRemoveBug(bugId) {
+    bugService
+      .remove(bugId)
+      .then(() => {
+        console.log('Deleted Succesfully!')
+        setBugs(prevBugs => prevBugs.filter((bug) => bug._id !== bugId))
+        showSuccessMsg('Bug removed')
+      })
+      .catch((err) => {
+        console.log('Error from onRemoveBug ->', err)
+        showErrorMsg('Cannot remove bug')
+      })
+  }
+
+  function onAddBug() {
+    const bug = {
+      title: prompt('Bug title?'),
+      severity: +prompt('Bug severity?'),
+    }
+    bugService
+      .save(bug)
+      .then((savedBug) => {
+        console.log('Added Bug', savedBug)
+        setBugs(prevBugs => [...prevBugs, savedBug])
+        showSuccessMsg('Bug added')
+      })
+      .catch((err) => {
+        console.log('Error from onAddBug ->', err)
+        showErrorMsg('Cannot add bug')
+      })
+  }
+
+  function onEditBug(bug) {
+    const severity = +prompt('New severity?')
+    const bugToSave = { ...bug, severity }
+    bugService
+      .save(bugToSave)
+      .then((savedBug) => {
+        console.log('Updated Bug:', savedBug)
+        setBugs(prevBugs => prevBugs.map((currBug) =>
+          currBug._id === savedBug._id ? savedBug : currBug
+        ))
+        showSuccessMsg('Bug updated')
+      })
+      .catch((err) => {
+        console.log('Error from onEditBug ->', err)
+        showErrorMsg('Cannot update bug')
+      })
+  }
+
+  return (
+    <main>
+      <h3>Bugs App</h3>
+      <main>
+        <button onClick={onAddBug}>Add Bug ⛐</button>
+        <BugList bugs={bugs} onRemoveBug={onRemoveBug} onEditBug={onEditBug} />
+      </main>
+    </main>
+  )
+}
